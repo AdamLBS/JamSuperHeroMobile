55c7406512ac7fc36d8af57b0e583e9c4532844c
Adam <adam@admlbs.fr>
2023-02-12 22:33:09 +0100

    images and desc

--- lib/results.dart
+++ lib/results.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:jam_epitech/colorType.dart';
 import 'globals.dart' as globals;
@@ -11,39 +13,57 @@ class ResultsPage extends StatefulWidget {
 
 class _ResultsPageState extends State<ResultsPage> {
   String result = "";
+  String desc = "";
   int score = 0;
+  String picValue = "";
   @override
   void initState() {
     if (globals.lex > score) {
       score = globals.lex;
+      desc = globals.lexDesc;
+      picValue = "assets/lex.png";
       result = "Lex";
     }
     if (globals.spider > score) {
       score = globals.spider;
+      desc = globals.spidermanDesc;
+      picValue = "assets/spider.png";
       result = "Spiderman";
     }
     if (globals.superman > score) {
       score = globals.superman;
+      desc = globals.supermanDesc;
+      picValue = "assets/superman.png";
       result = "Superman";
     }
     if (globals.mandela > score) {
       score = globals.mandela;
+      desc = globals.mandelaDesc;
+      picValue = "assets/mandela.png";
       result = "Mandela";
     }
     if (globals.joker > score) {
       score = globals.joker;
+      desc = globals.jokderDesc;
+      picValue = "assets/joker.png";
       result = "Joker";
     }
     if (globals.venom > score) {
       score = globals.venom;
+      desc = globals.venomDesc;
+      picValue = "assets/venom.png";
       result = "Venom";
     }
     if (globals.batman > score) {
       score = globals.batman;
+      desc = globals.batmanDesc;
+      picValue = "assets/batman.png";
       result = "Batman";
     }
     if (globals.thanos > score) {
       score = globals.thanos;
+      desc = globals.thanosDesc;
+      picValue = "assets/thanos.png";
       result = "Thanos";
     }
     // TODO: implement initState
@@ -92,6 +112,26 @@ class _ResultsPageState extends State<ResultsPage> {
                         fontFamily: 'Poppins',
                         fontStyle: FontStyle.italic,
                         fontSize: 40,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w800,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Image.asset(
+                  picValue,
+                  width: 100,
+                  height: 100,
+                ),
+                SingleChildScrollView(
+                  child: Container(
+                    child: Text(
+                      desc.toUpperCase(),
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontStyle: FontStyle.italic,
+                        fontSize: 15,
                         color: Colors.white,
                         fontWeight: FontWeight.w800,
                       ),
